d69ce5a335f1d0031bbdb1dfb03da7d00dc7eb49
laucss <laucubsan@alumn.us.es>
2026-04-30 19:52:28 +0200

    consulta repo

--- src/main/java/acme/features/manager/project/member/ProjectMemberRepository.java
+++ src/main/java/acme/features/manager/project/member/ProjectMemberRepository.java
@@ -1,9 +1,17 @@
 
 package acme.features.manager.project.member;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import acme.client.repositories.AbstractRepository;
+
 @Repository
-public interface ProjectMemberRepository {
+public interface ProjectMemberRepository extends AbstractRepository {
+
+	@Query("select pm FROM ProjectMember pm where pm.project.id = :projectId")
+	List<ProjectMember> findProjectMembersById(int projectId);
 
 }
