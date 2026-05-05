b041aa50aed5f938461106945c5b01cc174587e9
luciabaltasar2 <luciabaltasar2@gmail.com>
2026-05-05 19:11:50 +0200

    Cambios en any

--- src/main/java/acme/features/any/invention/AnyInventionRepository.java
+++ src/main/java/acme/features/any/invention/AnyInventionRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
 import acme.entities.inventions.Invention;
+import acme.entities.projects.Project;
 
 @Repository
 public interface AnyInventionRepository extends AbstractRepository {
@@ -17,5 +18,11 @@ public interface AnyInventionRepository extends AbstractRepository {
 
 	@Query("select i from Invention i where i.draftMode = false")
 	Collection<Invention> findPublishedInventions();
+
+	@Query("select i from Invention i where i.project.id = :projectId")
+	Collection<Invention> findInventionsByProjectId(int projectId);
+
+	@Query("select p from Project p where p.id = :id")
+	Project findProjectById(int id);
 
 }
